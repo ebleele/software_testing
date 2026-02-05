ac785d188651c4ab6095274102bcdc862509acba
ebleele <ellambehan@gmail.com>
2026-02-05 22:40:09 +0000

    testing CI pipeline

--- src/test/java/com/ilp2_ella_behan/GeometrySafetyTest.java
+++ src/test/java/com/ilp2_ella_behan/GeometrySafetyTest.java
@@ -53,6 +53,6 @@ public class GeometrySafetyTest {
         var poly = TestData.squareRestrictedArea(55.945000,-3.188000,0.00025).getVertices();
         var start = new Position(55.945000, -3.188800);
         var end = new Position(55.945000, -3.18800 - 0.00025);
-        assertFalse(Geometry.pathCrossesRegion(start,end,poly));
+        assertTrue(Geometry.pathCrossesRegion(start,end,poly));
     }
 }
